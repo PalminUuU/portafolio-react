fd36b8ae48d9e0c25ca53fa8cf2c7f0fe750c955
José Antonio Malpica Berthely <jose.malpica@conavimex.local>
2023-08-23 16:19:40 -0600

    principal

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,25 @@
-import $, { each, valHooks } from 'jquery';
-import React, { useState, useEffect } from 'react';
-import NavBar from '../components/NavBar';
+import $, { each, valHooks } from "jquery";
+import React, { useState, useEffect } from "react";
+import NavBar from "../components/NavBar";
 import "../assets/css/general.css";
 
 function HomePage(props) {
-
-    return (
-
-        <div className="Inicio">
-            <NavBar/>
-                <div className='div_principal'>
-                    <div className="content-container">
-                    <h1>Aqui ira Todo el contenido sobre los conocimientos</h1>
-                    </div>
-                </div>
+  return (
+    <div className="Inicio">
+      <NavBar />
+      <div className="color-des">
+        <div className="blur-background"></div>
+        <div className="div_principal content-container">
+        <div className="profile-picture"></div>
+          <h2>Hola!</h2>
+          <div className="color-nombre">
+            <h1>Mi nombre es Daniel Palma</h1>
+          </div>
+          <h2>Desarrollador Frontend y Backend</h2>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
