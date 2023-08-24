b63e57535c3c35ef4afb04b1df93063bbda8faa4
José Antonio Malpica Berthely <jose.malpica@conavimex.local>
2023-08-24 12:54:51 -0600

    estructura seccion2 mediaquery

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 import $, { each, valHooks } from "jquery";
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import NavBar from "../components/NavBar";
 import "../assets/css/general.css";
+import 'boxicons'
 
 function HomePage(props) {
   useEffect(() => {
@@ -9,21 +10,22 @@ function HomePage(props) {
       const visibleDivs = [];
 
       // Obtener todos los elementos DIV que son visibles
-      $("div[id^='seccion_']").each(function() {
+      $("div[id^='seccion_']").each(function () {
         if (esVisible(this)) {
           visibleDivs.push(this.id);
         }
       });
       $.each(visibleDivs, function (key, val) {
-       if (val == "seccion_acerca"){
-        $("#nav_incio").removeClass("active")
-        $("#nav_acerca").addClass("active")
-       }if (val == "seccion_incio"){
-        $("#nav_incio").addClass("active")
-        $("#nav_acerca").removeClass("active")
-       }
-      })
-       // IDs de los divs visibles
+        if (val == "seccion_acerca") {
+          $("#nav_incio").removeClass("active");
+          $("#nav_acerca").addClass("active");
+        }
+        if (val == "seccion_incio") {
+          $("#nav_incio").addClass("active");
+          $("#nav_acerca").removeClass("active");
+        }
+      });
+      // IDs de los divs visibles
     });
 
     $(window).on("scroll", () => {
@@ -42,14 +44,17 @@ function HomePage(props) {
       }
     });
 
-    $("a[href^='#']").on("click", function(event) {
+    $("a[href^='#']").on("click", function (event) {
       event.preventDefault();
 
       const target = $(this.getAttribute("href"));
       if (target.length) {
-        $("html, body").stop().animate({
-          scrollTop: target.offset().top
-        }, 1000); // Duración de la animación en milisegundos
+        $("html, body").stop().animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000
+        ); // Duración de la animación en milisegundos
       }
     });
   }, []);
@@ -58,12 +63,13 @@ function HomePage(props) {
     const rect = elemento.getBoundingClientRect();
     return (
       rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight)
     );
   };
   return (
     <div className="Inicio">
-      <NavBar/>
+      <NavBar />
       <div>
         <div>
           <div className="color-des">
@@ -74,21 +80,90 @@ function HomePage(props) {
               <div className="color-nombre">
                 <h1>Mi nombre es Daniel Palma</h1>
               </div>
-              <h2>Desarrollador Frontend y Backend</h2>
+              <h2>Desarrollador FullStack</h2>
             </div>
           </div>
         </div>
 
         <div id="seccion_acerca">
-  <div>
-  <h1>Acerca de mi</h1>
-    <div className="header-container">
-      <h3>¿quien soy?</h3>
-      <h3>Aptitudes</h3>
-    </div>
+          <div>
+            <div className="tittle">
+              <h1>Acerca de mi</h1>
+            </div>
+            <div className="text_container">
+              <div>
+                <h3>¿Quien soy?</h3>
+                <h5 className="contaier_cards">Mi nombre es Daniel Palma, Desarrollador FullStack</h5>
+                <h5>Ingeniero en Sistemas computacionales</h5>
+                <div className="contaier_cards text_long">Soy Desarrollador web con una gran capacidad para adaptarse a diferentes situaciones laborales, amo programar
+                por lo mismo siempre busco aprender nuevos lenguajes y tecnologias, no me cierro a aprender algo nuevo por que me caracterizo por aprender muy rapido
+                y eso mismo me ah llevado a manejar diferentes entornos de desarrollo, mi objetivo siempre sera el aprender y sobresalir.
+                </div>
+              </div>
+              <div className="containerl">
+                <div>
+                <h3>Conocimientos</h3>
+                <h5 className="contaier_cards">Tecnologias que manejo</h5>
 
-  </div>
-</div>
+                <div className="logos">
+
+                  <div className="logo_container">
+                 <box-icon name='html5' size='70px' type='logo' animation='flashing' color='#f36b07'></box-icon>
+                 <span>HTML5</span>
+                 </div>
+
+                <div className="logo_container">
+                <box-icon name='css3' type='logo' size='70px' animation='flashing' color='#0782f3' ></box-icon>
+                <span>CSS3</span>
+                </div>
+
+                <div className="logo_container">
+                <box-icon name='javascript' size='70px' type='logo' animation='flashing' color='#f3dd07' ></box-icon>
+                <span>Javascript</span>
+                </div>
+
+                <div className="logo_container">
+                <box-icon name='react' size='70px' type='logo' animation='flashing' color='#07aef3' ></box-icon>
+                <span>React</span>
+                </div>
+
+                <div className="logo_container">
+                <box-icon name='nodejs' size='70px' type='logo' animation='flashing' color='#22c114' ></box-icon>
+                <span>Node JS</span>
+                </div>
+
+                <div className="logo_container">
+                <box-icon name='postgresql' size='70px' type='logo' animation='flashing' color='#13709e' ></box-icon>
+                <span>Postgresql</span>
+                </div>
+
+                <div className="logo_container">
+                <box-icon name='data' size='70px' type='solid' animation='flashing' color='#2595c7' ></box-icon>
+                <span>Mysql</span>
+                </div>
+
+                <div className="logo_container">
+                <box-icon name='java' size='70px' type='logo' animation='flashing' color='#ef0a29' ></box-icon>
+                <span>Java</span>
+                </div>
+
+                <div className="logo_container">
+                <box-icon name='android'size='70px' type='logo' animation='flashing' color='#1eef0a' ></box-icon>
+                <span>Android</span>
+                </div>
+
+                <div className="logo_container">
+                <box-icon name='git' size='70px' type='logo' animation='flashing' color='#f51229' ></box-icon>
+                <span>GIT</span>
+                </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
       </div>
     </div>
   );
